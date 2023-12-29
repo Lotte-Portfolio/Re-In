6e25c60a41e6cef97d367e4a8214279784c97ff8
RosieOh <20172207@gm.hannam.ac.kr>
2023-12-30 03:26:44 +0900

    🧘‍♀️build : Board 기본 Type까지는 완🧘‍♀️

---
+++ src/main/java/com/rein/ReInApplication.java
@@ -0,0 +1,13 @@
+package com.rein;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReInApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReInApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rein/ReInApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rein;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReInApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
